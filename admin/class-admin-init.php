771e68e96e100d5574143b5d409e6af153206959
Varun Sridharan <varunsridharan23@gmail.com>
2015-09-07 17:57:50 +0530

    Base Commit
    Base Commit

---
+++ admin/class-admin-init.php
@@ -0,0 +1,115 @@
+<?php
+/**
+ * The admin-specific functionality of the plugin.
+ *
+ * @link       https://wordpress.org/plugins/woocommerce-role-based-price/
+ *
+ * The admin-specific functionality of the plugin.
+ *
+ * Defines the plugin name, version, and two examples hooks for how to
+ * enqueue the admin-specific stylesheet and JavaScript.
+ *
+ * @package    @TODO
+ * @subpackage @TODO
+ * @author     Varun Sridharan <varunsridharan23@gmail.com>
+ */
+if ( ! defined( 'WPINC' ) ) { die; }
+
+class WooCommerce_Plugin_Boiler_Plate_Admin extends WooCommerce_Plugin_Boiler_Plate {
+
+    /**
+	 * Initialize the class and set its properties.
+	 * @since      0.1
+	 */
+	public function __construct() {
+        self::func()->add_action( 'admin_enqueue_scripts', array( $this, 'enqueue_styles' ),99);
+        self::func()->add_action( 'admin_enqueue_scripts', array( $this, 'enqueue_scripts' ) );
+        self::func()->add_filter( 'plugin_row_meta', array($this, 'plugin_row_links' ), 10, 2 );
+        self::func()->add_action( 'admin_init', array( $this, 'admin_init' ));
+        self::func()->add_action( 'plugins_loaded', array( $this, 'init' ) );
+
+        
+	}
+
+    /**
+     * Inits Admin Sttings
+     */
+    public function admin_init(){
+        new WooCommerce_Plugin_Boiler_Plate_Admin_Settings;
+    }
+    
+    
+    public function init(){ 
+        self::func()->add_filter( 'woocommerce_integrations', array( $this, 'add_integration' ) );
+    }
+    
+	/**
+	 * Add a new integration to WooCommerce.
+	 */
+	public function add_integration( $integrations ) {
+		$integrations[] = 'WooCommerce_Plugin_Boiler_Plate_Settings_Intergation';
+		return $integrations;
+	}
+    
+    /**
+	 * Register the stylesheets for the admin area.
+	 */
+	public function enqueue_styles() { 
+        if(in_array($this->current_screen() , $this->get_screen_ids())) {
+            wp_enqueue_style(PLUGIN_SLUG.'_core_style',plugins_url('css/style.css',__FILE__) , array(), $this->version, 'all' );  
+        }
+	}
+	
+    
+    /**
+	 * Register the JavaScript for the admin area.
+	 */
+	public function enqueue_scripts() {
+        if(in_array($this->current_screen() , $this->get_screen_ids())) {
+            wp_enqueue_script(PLUGIN_SLUG.'_core_script', plugins_url('js/script.js',__FILE__), array('jquery'), $this->version, false ); 
+        }
+ 
+	}
+    
+    /**
+     * Gets Current Screen ID from wordpress
+     * @return string [Current Screen ID]
+     */
+    public function current_screen(){
+       $screen =  get_current_screen();
+       return $screen->id;
+    }
+    
+    /**
+     * Returns Predefined Screen IDS
+     * @return [Array] 
+     */
+    public function get_screen_ids(){
+        $screen_ids = array();
+        $screen_ids[] = 'edit-product';
+        $screen_ids[] = 'product';
+        return $screen_ids;
+    }
+    
+    
+    /**
+	 * Adds Some Plugin Options
+	 * @param  array  $plugin_meta
+	 * @param  string $plugin_file
+	 * @since 0.11
+	 * @return array
+	 */
+	public function plugin_row_links( $plugin_meta, $plugin_file ) {
+		if ( PLUGIN_FILE == $plugin_file ) {
+            $plugin_meta[] = sprintf('<a href="%s">%s</a>', '#', $this->__('Settings') );
+            $plugin_meta[] = sprintf('<a href="%s">%s</a>', '#', $this->__('F.A.Q') );
+            $plugin_meta[] = sprintf('<a href="%s">%s</a>', '#', 'View On Github' );
+            $plugin_meta[] = sprintf('<a href="%s">%s</a>', '#', $this->__('Report Issue') );
+            $plugin_meta[] = sprintf('&hearts; <a href="%s">%s</a>', '#', $this->__('Donate') );
+            $plugin_meta[] = sprintf('<a href="%s">%s</a>', 'http://varunsridharan.in/plugin-support/', $this->__('Contact Author') );
+		}
+		return $plugin_meta;
+	}	    
+}
+
+?>
